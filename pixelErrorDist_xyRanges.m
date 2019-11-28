963a4a4d929723bfd20fd6790c28d4acd299c92d
Elliott Huangfu <hfyxin@gmail.com>
2019-11-27 23:14:08 -0500

    Add a summary table, make graph nicer.

--- pixelErrorDist_xyRanges.m
+++ pixelErrorDist_xyRanges.m
@@ -1,17 +1,19 @@
 %% Plot distribution curves for a grid of XY range (in pixel).
 %
+% Suggest checking pixelErrorDist_yRanges.m before this one. It's easier.
 % Created by Elliott, based on Ben's files:
 %   - sortPixelErrorsXdistributionWithX_full.m
 %   - sortPixelErrorsYdistributionWithX_full.m
-% 
+% The change is verified by comparing the result from Ben's code.
+
 % The original image of 640 * 512 is divided into 2 * 3 grid (range):
-%               +-----------------------------+   --> (x direction)
-%    the image: | range 1 | range 2 | range 3 |
-%               +---------+---------+---------+
-%               | range 4 | range 5 | range 6 |
-% (y direction) +-----------------------------+ 
+%               +--------------------------+   --> (x direction)
+%    the image: | range1 | range2 | range3 |
+%               +--------+--------+--------+
+%               | range4 | range5 | range6 |
+% (y direction) +--------------------------+ 
 %
-% This script is to show that horizontal grids have similar error distribution.
+% This script is to show if horizontal grids have similar error distribution.
 %
 
 %% Load coordinate pairs
@@ -21,7 +23,8 @@ load('.\results\WCPairTable.mat')     % WCPairTable, WCS: world coordinate syste
 load('.\results\pixPairTable.mat')    % pixPairTable, 
 
 %% Pre-calculated values to divide the image
-range_y = [1,324,512]';
+%range_y = [1,324,512]';
+range_y = [1,175,512]';
 range_x = [1,214,427,640]';
 
 %% init some parameters
@@ -35,54 +38,56 @@ pixErrY = cell(n_ranges, 1);      % y pixel error array
 pixErrX = cell(n_ranges, 1);      % x pixel error array
 pixErrDistY = cell(n_ranges, 1);  % distribution function for y
 pixErrDistX = cell(n_ranges, 1);  % distribution function for x
-% pre-set text for all ranges (for plotting)
-range_txt = cell(n_ranges, 1);
+
+% pre-set label text for all ranges (for plotting)
+range_txt = cell(n_ranges, 1);   % cell, contains x, y range in str format.
 for r = 1:grid_r
-	for c = 1:grid_c
-		i = (r-1)*grid_c + c;
-		y_txt = ['[', num2str(range_y(r)), ',', num2str(range_y(r+1)), ')'];
-		x_txt = ['[', num2str(range_x(c)), ',', num2str(range_x(c+1)), ')'];
-		range_txt{i} = {x_txt; y_txt};
-	end
+    for c = 1:grid_c
+        i = (r-1)*grid_c + c;
+        y_txt = ['[', num2str(range_y(r)), ',', num2str(range_y(r+1)), ')'];
+        x_txt = ['[', num2str(range_x(c)), ',', num2str(range_x(c+1)), ')'];
+        range_txt{i} = {x_txt; y_txt};
+    end
 end
 
 
 %% record x and y pixel error values within each range
 for row = 1:height(pixPairTable)
-	for col = 1:width(pixPairTable)
-		% get the table cell
+    for col = 1:width(pixPairTable)
+        % get the table cell
         detection = pixPairTable{row, col}{1};  % cell array, don't forget the last {1}
         [cell_r, cell_c] = size(detection);
 
-		% Possible values of detection cell array:
-		%                     | cell size | detection[1] | detection[2] |
-		%                     |   r, c    | Ground Truth |  Predicted   |
-		% --------------------+-----------+--------------+--------------+
-		%  Nothing detected   |   1 x 1   |     []       |      null    |  
-		% One object detected |   1 x 2   |  1 x 2 array |  1 x 2 array |
-		% Multiple detections |   1 x 2   |  n x 2 array |  n x 2 array |
-		
-		% skip this cell if contains nothing
-		if cell_c == 1
-			continue
-		end
-		
-		% detection found, extract ground truth and detection
-		gt = detection{1};     % n x 2 array
-		pred = detection{2};   % n x 2 array
-		for i = 1:size(gt,1)   % iterate through i rows
-			x_pix = pred(i,1); % x coordinate, use detected value
-			y_pix = pred(i,2); % y coordinate
-			% find range (grid) index
-			idx_x = find(range_x > x_pix, 1) - 1;
-			idx_y = find(range_y > y_pix, 1) - 1;
-			idx = (idx_y - 1) * grid_c + idx_x;  % e.g.: id(1,2) is converted to 2
-			x_err = pred(i,1) - gt(i,1);         % pixel error on x direction
-			y_err = pred(i,2) - gt(i,2);         % pixel error on y direction
-			pixErrX{idx} = [pixErrX{idx}; x_err];
+        % Possible values of detection cell array:
+        %                     | cell size | detection[1] | detection[2] |
+        %                     |   r, c    | Ground Truth |  Predicted   |
+        % --------------------+-----------+--------------+--------------+
+        %  Nothing detected   |   1 x 1   |     []       |      null    |  
+        % One object detected |   1 x 2   |  1 x 2 array |  1 x 2 array |
+        % Multiple detections |   1 x 2   |  n x 2 array |  n x 2 array |
+        
+        % skip this cell if contains nothing
+        if cell_c == 1
+            continue
+        end
+        
+        % detection found, extract ground truth and detection
+        gt = detection{1};     % n x 2 array
+        pred = detection{2};   % n x 2 array
+        for i = 1:size(gt,1)   % iterate through i rows
+            y_pix = pred(i,2); % y coordinate, use detected value
+            x_pix = pred(i,1); % x coordinate, use detected value
+            % find grid index, then range index
+            idx_x = find(range_x > x_pix, 1) - 1;
+            idx_y = find(range_y > y_pix, 1) - 1;
+            idx = (idx_y - 1) * grid_c + idx_x;  % e.g.: id(1,2) is converted to 2
+            % record pixel error
+            x_err = pred(i,1) - gt(i,1);         % pixel error on x direction
+            y_err = pred(i,2) - gt(i,2);         % pixel error on y direction
+            pixErrX{idx} = [pixErrX{idx}; x_err];
             pixErrY{idx} = [pixErrY{idx}; y_err];
-		end
-	end
+        end
+    end
 end
 
 
@@ -103,6 +108,7 @@ for i = 1:n_ranges
     ax.YGrid = 'on';
 end
 
+
 %% Plot X direction pixel error histograms, separate plot
 figure
 n_row = hist_grid(1); n_col = hist_grid(2);  % number of plots on each row, col
@@ -121,4 +127,117 @@ for i = 1:n_ranges
 end
 
 
+%% Fit pixel error values in each range to probability function (PDF)
+for i = 1:n_ranges
+    pixErrDistY{i} = fitdist(pixErrY{i}, 'Normal');
+    pixErrDistX{i} = fitdist(pixErrX{i}, 'Normal');
+end
 
+
+%% find z score. Need to compare both horizontally and vertically
+% prepare some variables
+n_objects = zeros(n_ranges, 1);    % number of objects in each range
+mu_y = zeros(n_ranges, 1);
+mu_x = zeros(n_ranges, 1);
+sigma_y = zeros(n_ranges, 1);
+sigma_x = zeros(n_ranges, 1);
+for i = 1:n_ranges
+    n_objects(i) = length(pixErrY{i});
+    mu_y(i) = pixErrDistY{i}.mu;
+    mu_x(i) = pixErrDistX{i}.mu;
+    sigma_y(i) = pixErrDistY{i}.sigma;
+    sigma_x(i) = pixErrDistX{i}.sigma;
+end
+
+% z score init
+z_score_hori_y = zeros(n_ranges, 1);     % horizontally, w.r.t the next grid
+z_score_hori_x = zeros(n_ranges, 1);     % horizontally, w.r.t the next grid
+z_score_vert_y = zeros(n_ranges, 1);     % vertically, w.r.t the next grid
+z_score_vert_x = zeros(n_ranges, 1);     % vertically, w.r.t the next grid
+
+for i = 1:n_ranges
+    % skip the last col, when calculating horizontal z score
+    if rem(i, grid_c) == 0
+        z_score_hori_y(i) = nan;
+        z_score_hori_x(i) = nan;
+    else
+        % calculate horizontally adjacent grid (e.g., range1 vs range2)
+        z_score_hori_y(i) = (mu_y(i) - mu_y(i+1)) / ...
+            sqrt(sigma_y(i)^2/n_objects(i) + sigma_y(i+1)^2/n_objects(i+1)); 
+        z_score_hori_x(i) = (mu_x(i) - mu_x(i+1)) / ...
+            sqrt(sigma_x(i)^2/n_objects(i) + sigma_x(i+1)^2/n_objects(i+1)); 
+    end
+    % skip the last row, when calculating vertical z score
+    if i > n_ranges - grid_c
+        z_score_vert_y(i) = nan;
+        z_score_vert_x(i) = nan;
+    else
+        % calculate vertically adjacent grid (e.g., range1 vs range4)
+        z_score_vert_y(i) = (mu_y(i) - mu_y(i+grid_c)) / ...
+            sqrt(sigma_y(i)^2/n_objects(i) + sigma_y(i+grid_c)^2/n_objects(i+grid_c)); 
+        z_score_vert_x(i) = (mu_x(i) - mu_x(i+grid_c)) / ...
+            sqrt(sigma_x(i)^2/n_objects(i) + sigma_x(i+grid_c)^2/n_objects(i+grid_c)); 
+    end
+end
+
+
+%% Summarize what we've got so far into a table
+% note the var names begins with Capital to look better.
+Range = (1:n_ranges)';    % range ID
+XRange = cell(n_ranges,1);  % str text cell
+YRange = cell(n_ranges,1);  % str text cell
+for i = 1:n_ranges
+    XRange{i} = range_txt{i}{1};
+    YRange{i} = range_txt{i}{2};
+end
+% the summary table
+t_dist = table(Range,XRange,YRange,n_objects,...
+                mu_y,sigma_y,z_score_hori_y,z_score_vert_y,...
+                mu_x,sigma_x,z_score_hori_x,z_score_vert_x);
+t_dist
+
+
+%% Plot Y direction pixel error distribution curve, single plot
+figure
+x_ = -40:0.1:60;
+legend_val = cell(1,n_ranges);
+for i = 1:n_ranges
+    % plot PDF
+    y_ = pdf(pixErrDistY{i}, x_);
+    plot(x_, y_, 'LineWidth', 2);
+    legend_txt = ['Range: X=', range_txt{i}{1}, ' Y=', range_txt{i}{2}];
+    legend_val{i} = legend_txt;
+    hold on
+    % plot mu
+    ver_line = pixErrDistY{i}.mu;
+    % xline(ver_line, '--k');   % only in MATLAB 2018b or higher
+end
+title('Fitted PDF for errors on Y direction');
+legend(legend_val,'Location', 'NorthEast');
+xlabel('pixel error');
+ylabel('probability');
+grid on
+
+%% Plot X direction pixel error distribution curve, single plot
+figure
+x_ = -40:0.1:60;
+legend_val = cell(1,n_ranges);
+for i = 1:n_ranges
+    % plot PDF
+    y_ = pdf(pixErrDistX{i}, x_);
+    plot(x_, y_, 'LineWidth', 2);
+    legend_txt = ['Range: X=', range_txt{i}{1}, ' Y=', range_txt{i}{2}];
+    legend_val{i} = legend_txt;
+    hold on
+    % plot mu
+    ver_line = pixErrDistY{i}.mu;
+    % xline(ver_line, '--k');   % only in MATLAB 2018b or higher
+end
+title('Fitted PDF for errors on X direction');
+legend(legend_val,'Location', 'NorthEast');
+xlabel('pixel error');
+grid on
+
+%% remove some temp vars
+clearvars -except pixPairTable WCPairTable range_y range_x hist_grid n_ranges ...
+                  pixErrX pixErrY pixErrDistX pixErrDistY t_dist range_txt
--- pixelErrorDist_yRanges.m
+++ pixelErrorDist_yRanges.m
@@ -1,13 +1,13 @@
-%% Plot distribution curve for each Y distance range (in pixel).
+%% Plot distribution curves for each Y distance range (in pixel).
 %
-% Created by Elliott, based on Ben's files:
+% Created by Elliott, based on Ben's files, but with some addition.
 %   - sortPixelErrorsXdistYpixels_full.m
 %   - sortPixelErrorsYdistribution_full.m
 % The change is verified by comparing the result from Ben's code.
 % However, some table does not perfect match the paper draft (but close).
-% Changes made to Ben's program:
-%   - Add a table for better showing
-%   - Add mu, sigma for errors on X direction
+%
+% This script is to show that error distributions vary by distance (y range).
+%
 
 %% Load coordinate pairs
 % pair_value looks like: [ground truth, detected]
@@ -15,7 +15,7 @@ clear
 load('.\results\WCPairTable.mat')     % WCPairTable, WCS: world coordinate system
 load('.\results\pixPairTable.mat')    % pixPairTable, 
 
-%% Pre-calculated values, don't forget it starts from 1
+%% Pre-calculated values to divide the image, don't forget it starts from 1
 % original pixel range, calculated by WCS range [1,5,10,15, ...,55,60,75,90,inf]
 range_y_original = [1 161 165 168 172 175 178 182 188 195 206 223 253 324 512]';   % 14 ranges
 % the range from Ben's code sortPixelErrorsYdistribution_full.m
@@ -39,36 +39,38 @@ pixErrDistX = cell(n_ranges, 1);  % distribution function for x
 
 %% record x and y pixel error values within each range
 for row = 1:height(pixPairTable)
-	for col = 1:width(pixPairTable)
-		% get the table cell
+    for col = 1:width(pixPairTable)
+        % get the table cell
         detection = pixPairTable{row, col}{1};  % cell array, don't forget the last {1}
         [cell_r, cell_c] = size(detection);
 
-		% Possible values of detection cell array:
-		%                     | cell size | detection[1] | detection[2] |
-		%                     |   r, c    | Ground Truth |  Predicted   |
-		% --------------------+-----------+--------------+--------------+
-		%  Nothing detected   |   1 x 1   |     []       |      null    |  
-		% One object detected |   1 x 2   |  1 x 2 array |  1 x 2 array |
-		% Multiple detections |   1 x 2   |  n x 2 array |  n x 2 array |
-		
-		% skip this cell if contains nothing
-		if cell_c == 1
-			continue
-		end
-		
-		% detection found, extract ground truth and detection
-		gt = detection{1};     % n x 2 array
-		pred = detection{2};   % n x 2 array
-		for i = 1:size(gt,1)   % iterate through i rows
-			y_pix = pred(i,2); % y coordinate, use detected value
-			idx = find(ranges > y_pix, 1) - 1;   % find range index, neat!
-			x_err = pred(i,1) - gt(i,1);         % pixel error on x direction
-			y_err = pred(i,2) - gt(i,2);         % pixel error on y direction
-			pixErrX{idx} = [pixErrY{idx}; x_err];
+        % Possible values of detection cell array:
+        %                     | cell size | detection[1] | detection[2] |
+        %                     |   r, c    | Ground Truth |  Predicted   |
+        % --------------------+-----------+--------------+--------------+
+        %  Nothing detected   |   1 x 1   |     []       |      null    |  
+        % One object detected |   1 x 2   |  1 x 2 array |  1 x 2 array |
+        % Multiple detections |   1 x 2   |  n x 2 array |  n x 2 array |
+        
+        % skip this cell if contains nothing
+        if cell_c == 1
+            continue
+        end
+        
+        % detection found, extract ground truth and detection
+        gt = detection{1};     % n x 2 array
+        pred = detection{2};   % n x 2 array
+        for i = 1:size(gt,1)   % iterate through i rows
+            y_pix = pred(i,2); % y coordinate, use detected value
+            % find range index, neat!
+            idx = find(ranges > y_pix, 1) - 1;
+            % record pixel error
+            x_err = pred(i,1) - gt(i,1);         % pixel error on x direction
+            y_err = pred(i,2) - gt(i,2);         % pixel error on y direction
+            pixErrX{idx} = [pixErrX{idx}; x_err];
             pixErrY{idx} = [pixErrY{idx}; y_err];
-		end
-	end
+        end
+    end
 end
 
 %% Plot Y direction pixel error histograms, separate plot
@@ -90,7 +92,7 @@ end
 %% Fit pixel error values in each range to probability function (PDF)
 for i = 1:n_ranges
     pixErrDistY{i} = fitdist(pixErrY{i}, 'Normal');
-	pixErrDistX{i} = fitdist(pixErrX{i}, 'Normal');
+    pixErrDistX{i} = fitdist(pixErrX{i}, 'Normal');
 end
 
 
@@ -104,18 +106,19 @@ sigma_x = zeros(n_ranges, 1);
 for i = 1:n_ranges
     n_objects(i) = length(pixErrY{i});
     mu_y(i) = pixErrDistY{i}.mu;
-	mu_x(i) = pixErrDistX{i}.mu;
+    mu_x(i) = pixErrDistX{i}.mu;
     sigma_y(i) = pixErrDistY{i}.sigma;
-	sigma_x(i) = pixErrDistX{i}.sigma;
+    sigma_x(i) = pixErrDistX{i}.sigma;
 end
 
+% z score init
 z_score_y = zeros(n_ranges, 1);     % w.r.t the next range
 z_score_x = zeros(n_ranges, 1);     % w.r.t the next range
 for i = 1:n_ranges
     % skip the last calculation
     if i == n_ranges
         z_score_y(i) = nan;
-		z_score_x(i) = nan;
+        z_score_x(i) = nan;
         continue
     end
     % calculation
@@ -128,15 +131,12 @@ end
 
 %% Summarize what we've got so far into a table
 % note the var names begins with Capital to look better.
-Range = (1:n_ranges)';
+Range = (1:n_ranges)';    % range ID
 PixelRangeStart = ranges(1:end-1);  % inclusive
 PixelRangeEnd = ranges(2:end);    % exclusive
 % the table used in paper
-t_dist = table(Range,PixelRangeStart,PixelRangeEnd,n_objects,mu_y,...
-				sigma_y,z_score_y, mu_x,sigma_x,z_score_x);
-% remove some temp vars
-clearvars -except pixPairTable WCPairTable ranges hist_grid n_ranges ...
-				  pixErrX pixErrY pixErrDistX pixErrDistY t_dist
+t_dist = table(Range,PixelRangeStart,PixelRangeEnd,n_objects,...
+                mu_y,sigma_y,z_score_y, mu_x,sigma_x,z_score_x);
 t_dist
 
 
@@ -144,8 +144,6 @@ t_dist
 figure
 x_ = -40:0.1:60;
 legend_val = cell(1,n_ranges);
-xlabel('pixel error');
-ylabel('# of detections');
 for i = 1:n_ranges
     % plot PDF
     y_ = pdf(pixErrDistY{i}, x_);
@@ -156,7 +154,11 @@ for i = 1:n_ranges
     ver_line = pixErrDistY{i}.mu;
     % xline(ver_line, '--k');   % only in MATLAB 2018b or higher
 end
-title('Fitted probability distribution functions');
+title('Fitted PDF for errors on Y direction');
 legend(legend_val,'Location', 'NorthEast');
 xlabel('pixel error');
 grid on
+
+%% remove some temp vars
+clearvars -except pixPairTable WCPairTable ranges hist_grid n_ranges ...
+                  pixErrX pixErrY pixErrDistX pixErrDistY t_dist
